627dcaed1c2cda9b34a0c3a680b3ccf1115bd51c
Amadeo-Alex <68441479+amadeo-alex@users.noreply.github.com>
2025-11-27 21:08:20 +0100

    le push

--- src/HASS.Agent.Client/ViewLocator.cs
+++ src/HASS.Agent.Client/ViewLocator.cs
@@ -7,27 +7,27 @@ namespace HASS.Agent.Client;
 
 public class ViewLocator : IDataTemplate
 {
-	public Control? Build(object? param)
-	{
-		if (param is null)
-			return null;
+    public Control? Build(object? param)
+    {
+        if (param is null)
+            return null;
 
-		var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-		var type = Type.GetType(name);
+        var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+        var type = Type.GetType(name);
 
-		if (type == null)
-		{
-			return new TextBlock { Text = "Not Found: " + name };
-		}
-		
-		var control = (Control)Activator.CreateInstance(type)!;
-		control.DataContext = param;
+        if (type == null)
+        {
+            return new TextBlock { Text = "Not Found: " + name };
+        }
 
-		return control;
-	}
+        var control = (Control)Activator.CreateInstance(type)!;
+        control.DataContext = param;
 
-	public bool Match(object? data)
-	{
-		return data is ViewModelBase;
-	}
+        return control;
+    }
+
+    public bool Match(object? data)
+    {
+        return data is ViewModelBase;
+    }
 }
